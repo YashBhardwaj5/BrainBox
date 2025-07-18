784a0d2cdcfc264f2ecb4400780a589c96614bd8
Yash <yashbhardwaj0307@gmail.com>
2025-07-19 02:20:54 +0530

    creating a component library

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ function App() {
 
   return (
     <>
-      
+      <div className='bg-blue-200 min-h-screen'>
+        <div className='container mx-auto bg-amber-200'>
+          <div className='max-w-2xl bg-red-200 mx-auto'>
+              tailwind setup
+          </div>
+        </div>
+      </div>
     </>
   )
 }
