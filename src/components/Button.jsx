848546261912dab11fba5ec5d6ae62179bd6b644
Yash <yashbhardwaj0307@gmail.com>
2025-07-20 03:40:44 +0530

    made General Buttons in Ui lib

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,14 @@
-export const Button=()=>{
-    return <button></button>
+
+const variantStyles={
+    "primary":"bg-indigo-600 text-white rounded-md mx-1",
+    "secondary":"bg-indigo-100 text-indigo-600 rounded-md mx-1"
+}
+const sizeStyles={
+    "sm":"py-1 px-2",
+    "md":"py-2 px-4",
+    "lg":"py-4 px-6",
+}
+export const Button=(props)=>{
+    const { variant, size, text, startIcon, onClick } = props;
+    return <button className={variantStyles[variant]+" "+sizeStyles[size]}>{text}</button>
 }
