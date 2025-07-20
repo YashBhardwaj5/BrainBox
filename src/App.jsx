dca42999825acd88a37ef450809e08f0704873e9
Yash <yashbhardwaj0307@gmail.com>
2025-07-20 23:00:21 +0530

    added share icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
 import { Button } from './components/Button'
+import { Plus } from './icons/Plusicon'
+import { ShareIcon } from './icons/ShareIcon'
+
 function App() {
 
   return (
@@ -8,9 +11,9 @@ function App() {
         <div className='container mx-auto'>
           <div className='max-w-2xl mx-auto'>
               tailwind setup
-              <Button variant="primary" size="sm" text="Small" />
-              <Button variant="secondary" size="md" text="Medium" />
-              <Button variant="secondary" size="lg" text="large" />
+              <Button variant="primary" size="sm" text="Small" startIcon={<Plus size="sm"/>}/>
+              <Button variant="secondary" size="md" text="Add Content" startIcon={<Plus size="md"/>}/>
+              <Button variant="secondary" size="lg" text="Add Content" startIcon={<ShareIcon size="lg"/>}/>
           </div>
         </div>
       </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,16 @@
 
 const variantStyles={
-    "primary":"bg-indigo-600 text-white rounded-md mx-1",
-    "secondary":"bg-indigo-100 text-indigo-600 rounded-md mx-1"
+    "primary":"bg-indigo-600 text-white",
+    "secondary":"bg-indigo-100 text-indigo-600"
 }
 const sizeStyles={
-    "sm":"py-1 px-2",
-    "md":"py-2 px-4",
-    "lg":"py-4 px-6",
+    "sm":"py-1 px-2 text-sm",
+    "md":"py-2 px-4 text-md",
+    "lg":"py-4 px-6 text-lg",
 }
+const defaultStyles=" rounded-md flex items-center"
+
 export const Button=(props)=>{
     const { variant, size, text, startIcon, onClick } = props;
-    return <button className={variantStyles[variant]+" "+sizeStyles[size]}>{text}</button>
-}
+    return <button className={variantStyles[variant]+" "+sizeStyles[size]+ defaultStyles}>{startIcon?<div className="pr-2">{startIcon}</div>:null}{text}</button>}
+
