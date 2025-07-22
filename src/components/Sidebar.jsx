de260ee8ac4929d913bc38b69a95a7e9d786856f
Yash <yashbhardwaj0307@gmail.com>
2025-07-22 15:08:27 +0530

    SideBar Done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,6 @@
+import { DocumentIcon } from "../icons/DocumentIcon"
+import { HashtagIcon } from "../icons/HashtagIcon"
+import { LinksIcon } from "../icons/LinksIcon"
 import { Logo } from "../icons/Logo"
 import { TwitterIcon } from "../icons/TwitterIcon"
 import { YoutubeIcon } from "../icons/YoutubeIcon"
@@ -5,13 +8,17 @@ import { SidebarItem } from "./SidebarItem"
 
 export const Sidebar=()=>{
     return <div className="h-screen bg-white border border-gray-100 w-72 absolute top-0 left-0 pl-6">
-            <div className="text-2xl font-medium pt-8 flex">
-                <Logo/>
-                BrainBox
+            <div className="text-3xl font-medium pt-8 flex">
+                <div className="text-purple-800 pr-1"><Logo/></div>
+                <div>BrainBox</div>
             </div>
-            <div className="pt-8 pl-4">
+            <div className="pt-8 pl-6">
                 <SidebarItem text="Twitter" icon={<TwitterIcon/>}></SidebarItem>
-                <SidebarItem text="Youtube" icon={<YoutubeIcon/>}></SidebarItem>
+                <SidebarItem text="Videos" icon={<YoutubeIcon/>}></SidebarItem>
+                <SidebarItem text="Documents" icon={<DocumentIcon/>}></SidebarItem>
+                <SidebarItem text="Links" icon={<LinksIcon/>}></SidebarItem>
+                <SidebarItem text="Tags" icon={<HashtagIcon/>}></SidebarItem>
+                
             </div>
     </div>
 }
--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -1,6 +1,6 @@
 export const SidebarItem=(props)=>{
     const {icon,text}=props;
-    return <div className="flex font-medium text-gray-800 py-2">
+    return <div className="flex font-medium text-gray-800 py-2 hover:bg-gray-100 w-48 rounded-md cursor-pointer mb-1">
         <div className="pr-2">{icon}</div>
         <div>{text}</div>
     </div>
